3bb7275359af672daf9cb999dd8e5de972f7d195
tgson069 <115939547+tgson069@users.noreply.github.com>
2022-12-01 16:16:44 +0700

    Delete 8.4.py

--- 8.4.py
+++
@@ -1,8 +0,0 @@
-import math
-
-a = int(input("canh thu nhat "))
-b = int(input("canh thu hai "))
-c = int(input("canh thu ba "))
-p=(a+b+c)/2
-h=math.sqrt(p*(p-a)*(p-c)*(p-b))
-print("dien tich tam giac la",h)
